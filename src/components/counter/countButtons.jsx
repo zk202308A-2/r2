b61dfd7e41cbab1cf6ca231430bd296142c9561f
inhawinter <inhawinter@gmail.com>
2025-06-25 15:13:18 +0900

    aaa

--- src/components/counter/countButtons.jsx
+++ src/components/counter/countButtons.jsx
@@ -1,10 +1,10 @@
 function CountButtons() {
     return ( 
         <div className="flex justify-center items-center">
-            <div> 
+            <div className="m-10 px-6 py-3 bg-red-500 text-white font-bold rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75 transition duration-200 ease-in-out"> 
                 + 
             </div>
-            <div> 
+            <div className="m-10 px-6 py-3 bg-red-500 text-white font-bold rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75 transition duration-200 ease-in-out"> 
                 - 
             </div>
         </div>
