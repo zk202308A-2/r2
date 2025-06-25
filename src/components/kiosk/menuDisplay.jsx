a251071255c57e8bee9ef084c43a1f77f68e3d59
inhawinter <inhawinter@gmail.com>
2025-06-25 14:42:43 +0900

    aaa

--- src/components/kiosk/menuDisplay.jsx
+++ src/components/kiosk/menuDisplay.jsx
@@ -1,12 +1,26 @@
-function MenuDisplay({arr}) {
-    return (
-        <>
-            <ul>
-                {arr.map( ({mno, name, price}) => 
-                    <li key={mno}>{mno} - {name} - {price}</li>)}
-            </ul>
-        </>
-      );
-}
+function MenuDisplay({ arr }) {
 
-export default MenuDisplay;
+    return (
+      <>
+        <ul>
+          {makeLi(arr)}
+        </ul>
+      </>
+    );
+  }
+
+  function makeLi(arr) {
+    return arr.map(({ mno, name, price }) =>
+    <li
+    key={mno}
+    className="bg-white rounded-lg shadow-md p-4 mb-4 flex justify-between items-center"
+    >
+    <div className="font-bold text-lg">{name}</div>
+    <div className="text-gray-700">No. {mno}</div>
+    <div className="text-blue-600 font-semibold">${price}</div>
+    </li>
+)
+
+  }
+  
+  export default MenuDisplay;
