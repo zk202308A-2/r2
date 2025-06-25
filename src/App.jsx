b1eb9e9715890f8ac5157c7117a0fd88576c0025
inhawinter <inhawinter@gmail.com>
2025-06-25 11:32:28 +0900

    dddds

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-4xl bg-amber-400 underline rounded-4xl'>
+        Hello World
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter>
     </>
   )
 }
---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+function Counter() {
+
+    return (
+        <div className="bg-amber-600 w-full h-1/3">
+            Counter 
+        </div>
+    )
+
+}
+
+export default Counter
+
