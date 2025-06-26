dd111a0e253a26012e6656f2a52d6352ecc6a132
inhawinter <inhawinter@gmail.com>
2025-06-26 13:45:25 +0900

    aaa

--- src/components/todo/todoInput.jsx
+++ src/components/todo/todoInput.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
 
 function TodoInput() {
+
+    const [data,setData] = useState({title:'AAAA', writer:'user00'})
+
+    const handleChange = (e) => {
+        console.log("event: " , e)
+        const target = e.target
+        const name = target.name
+        const value = target.value
+        console.log("name", name, "value",value)
+
+        data[name] = value
+
+        //const newData  = {...data}
+        //setData(() => newData)
+        
+        setData(() => ({...data}))
+    }
+
     return ( 
         <div>
             <div>
                 Title:
-                <input className="border-2 p-2 m-2" type="text" name='title'></input>
+                <input className="border-2 p-2 m-2" 
+                type="text" 
+                name='title'
+                value={data.title}
+                onChange={handleChange}></input>
             </div>
             <div>
                 Writer: 
-                <input type="text"  className="border-2 p-2 m-2" name='writer'></input>
+                <input type="text"  
+                className="border-2 p-2 m-2" 
+                name='writer'
+                value={data.writer}
+                onChange={handleChange}></input>
             </div>
             <div>
                 <button>CLICK</button>
