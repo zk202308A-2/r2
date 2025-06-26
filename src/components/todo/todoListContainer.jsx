33ad5b497e34d6fcf1d7f86872c089d1e1ee2ae2
inhawinter <inhawinter@gmail.com>
2025-06-26 15:05:26 +0900

    wsedde

--- src/components/todo/todoListContainer.jsx
+++ src/components/todo/todoListContainer.jsx
@@ -1,24 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 function TodoListContainer() {
 
     const [data, setData] = useState([])
+    const [page, setPage] = useState(1)
+    const [refresh, setRefresh] = useState(true)
 
     async function getData() {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`)
         const result = await res.json()
         setData(() => result)
     }
-    getData()
+
+    useEffect(() =>{
+        getData()
+    }, [page, refresh])
+
 
 
     return ( 
         <div>
             Todo List
             <ul>
-                {data.map(user => <li>AAAA</li>)}
+                {data.map(todo => <li key={todo.id}>{todo.id} --- {todo.title}</li>)}
             </ul>
+
+            <div onClick={() => {
+                if(page === 2){
+                    setRefresh(!refresh)
+                    return
+                }    
+
+                setPage(2)
+            }} >2페이지</div>
+            <div onClick={() => {
+                                if(page === 3){
+                                    setRefresh(!refresh)
+                                    return
+                                }    
+                
+                setPage(3)
+            }}>3페이지</div>
         </div>
      );
 }
